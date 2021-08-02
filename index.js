e991fc1bb9cd8dd91046d90726f6630579b5ff67
JAMart <jorge.martinez1597@gmail.com>
2021-08-01 20:10:36 -0400

    cleaned up files

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-
-
-
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
 const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
@@ -83,7 +71,6 @@ const createManager = () => {
         teamMembers.push(manager);
 
         console.log(manager);
-        console.log(teamMembers);
     })
     .then(createEmployee);
 
@@ -150,7 +137,6 @@ const createEngineer = function() {
         teamMembers.push(engineer);
 
         console.log(engineer);
-        console.log(teamMembers);
     })
     .then(createEmployee);
 };
@@ -216,7 +202,6 @@ const createIntern = function () {
         teamMembers.push(intern);
 
         console.log(intern);
-        console.log(teamMembers);
     })
     .then(createEmployee);
 };
